7eaa805acb98c09e32d781584777fd7ffe392a02
TJ Draper <tj@buzzingpixel.com>
2021-12-21 10:23:47 -0600

    Initialize project

---
+++ testBootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:disable PSR1.Files.SideEffects.FoundWithSymbols
+
+ini_set('display_errors', 'On');
+ini_set('html_errors', '0');
+error_reporting(-1);
+
+if (! class_exists(Yii::class)) {
+    include_once APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
+}
+
+if (! class_exists(Craft::class)) {
+    include_once APP_BASE_PATH . '/vendor/craftcms/cms/src/Craft.php';
+}
