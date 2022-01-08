45d94d12ff00d4eec7ad4bd4b75eef83268f81a6
TJ Draper <tj@buzzingpixel.com>
2022-01-08 17:01:31 -0600

    Add Craft Migration to add images table

---
+++ src/Migrate/Migrations/C0008AddImagesTable.php
@@ -0,0 +1,196 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BuzzingPixel\Ansel\Migrate\Migrations;
+
+use BuzzingPixel\Ansel\Migrate\MigrationContract;
+use craft\db\Connection as DbConnection;
+use yii\db\ColumnSchemaBuilder;
+use yii\db\Exception;
+use yii\db\Schema;
+
+class C0008AddImagesTable implements MigrationContract
+{
+    private DbConnection $db;
+
+    public function __construct(DbConnection $db)
+    {
+        $this->db = $db;
+    }
+
+    public function for(): string
+    {
+        return MigrationContract::CRAFT;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(): bool
+    {
+        if ($this->db->tableExists('{{%ansel_images}}')) {
+            return true;
+        }
+
+        $this->db->createCommand()->createTable(
+            '{{%ansel_images}}',
+            [
+                'id' => new ColumnSchemaBuilder(Schema::TYPE_PK),
+                'elementId' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                    11,
+                ))->notNull(),
+                'fieldId' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                    11,
+                ))->notNull(),
+                'userId' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                    11,
+                ))->notNull(),
+                'assetId' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                    11,
+                ))->notNull(),
+                'highQualAssetId' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                    11,
+                )),
+                'thumbAssetId' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                    11,
+                )),
+                'originalAssetId' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                    11,
+                )),
+                'width' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                ))->notNull(),
+                'height' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                ))->notNull(),
+                'x' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                ))->notNull(),
+                'y' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_INTEGER,
+                ))->notNull(),
+                'title' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_STRING,
+                    255
+                )),
+                'caption' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_STRING,
+                    255
+                )),
+                'cover' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_TINYINT,
+                    1
+                ))->notNull(),
+                'position' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_TINYINT,
+                    4
+                ))->notNull(),
+                'disabled' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_TINYINT
+                )),
+                'dateCreated' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_DATETIME,
+                ))->notNull(),
+                'dateUpdated' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_DATETIME,
+                ))->notNull(),
+                'uid' => (new ColumnSchemaBuilder(
+                    Schema::TYPE_CHAR,
+                    36,
+                ))->notNull()->defaultValue('0'),
+            ]
+        )->execute();
+
+        $this->db->createCommand()->addForeignKey(
+            $this->db->getForeignKeyName(),
+            '{{%ansel_images}}',
+            ['elementId'],
+            '{{%elements}}',
+            ['id'],
+            'RESTRICT',
+            'RESTRICT',
+        );
+
+        $this->db->createCommand()->addForeignKey(
+            $this->db->getForeignKeyName(),
+            '{{%anselImages}}',
+            ['fieldId'],
+            '{{%fields}}',
+            ['id'],
+            'RESTRICT',
+            'RESTRICT',
+        );
+
+        $this->db->createCommand()->addForeignKey(
+            $this->db->getForeignKeyName(),
+            '{{%anselImages}}',
+            ['userId'],
+            '{{%users}}',
+            ['id'],
+            'RESTRICT',
+            'RESTRICT',
+        );
+
+        $this->db->createCommand()->addForeignKey(
+            $this->db->getForeignKeyName(),
+            '{{%anselImages}}',
+            ['assetId'],
+            '{{%assets}}',
+            ['id'],
+            'SET NULL',
+            'RESTRICT',
+        );
+
+        $this->db->createCommand()->addForeignKey(
+            $this->db->getForeignKeyName(),
+            '{{%anselImages}}',
+            ['highQualAssetId'],
+            '{{%assets}}',
+            ['id'],
+            'SET NULL',
+            'RESTRICT',
+        );
+
+        $this->db->createCommand()->addForeignKey(
+            $this->db->getForeignKeyName(),
+            '{{%anselImages}}',
+            ['thumbAssetId'],
+            '{{%assets}}',
+            ['id'],
+            'SET NULL',
+            'RESTRICT',
+        );
+
+        $this->db->createCommand()->addForeignKey(
+            $this->db->getForeignKeyName(),
+            '{{%anselImages}}',
+            ['originalAssetId'],
+            '{{%assets}}',
+            ['id'],
+            'SET NULL',
+            'RESTRICT',
+        );
+
+        return true;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function down(): bool
+    {
+        $this->db->createCommand()->dropTableIfExists(
+            '{{%ansel_images}}',
+        )->execute();
+
+        return true;
+    }
+}
---
+++ src/Migrate/Migrations/C0008AddImagesTableTest.php
@@ -0,0 +1,481 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BuzzingPixel\Ansel\Migrate\Migrations;
+
+use BuzzingPixel\Ansel\Migrate\MigrationContract;
+use craft\db\Command;
+use craft\db\Connection as DbConnection;
+use PHPUnit\Framework\TestCase;
+use yii\db\ColumnSchemaBuilder;
+use yii\db\Exception;
+
+use function assert;
+use function is_array;
+
+class C0008AddImagesTableTest extends TestCase
+{
+    /** @var mixed[] */
+    private array $calls = [];
+
+    private bool $tableExistsReturn = false;
+
+    private C0008AddImagesTable $migration;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->migration = new C0008AddImagesTable($this->mockDb());
+    }
+
+    private function mockDb(): DbConnection
+    {
+        $mock = $this->createMock(DbConnection::class);
+
+        $mock->method('tableExists')->willReturnCallback(
+            function (string $table): bool {
+                $this->calls[] = [
+                    'object' => 'DbConnection',
+                    'method' => 'tableExists',
+                    'table' => $table,
+                ];
+
+                return $this->tableExistsReturn;
+            }
+        );
+
+        $mock->method('getForeignKeyName')->willReturn(
+            'fooBarForeign',
+        );
+
+        $mock->method('createCommand')->willReturnCallback(
+            function (): Command {
+                return $this->mockCommand();
+            }
+        );
+
+        return $mock;
+    }
+
+    private function mockCommand(): Command
+    {
+        $mock = $this->createMock(Command::class);
+
+        $mock->method('createTable')->willReturnCallback(
+            function (
+                string $table,
+                array $columns
+            ) use ($mock): Command {
+                $this->calls[] = [
+                    'object' => 'Command',
+                    'method' => 'createTable',
+                    'table' => $table,
+                    'columns' => $columns,
+                ];
+
+                return $mock;
+            }
+        );
+
+        $mock->method('addForeignKey')->willReturnCallback(
+            function (
+                string $name,
+                string $table,
+                array $columns,
+                string $refTable,
+                array $refColumns,
+                string $delete,
+                string $update
+            ) use ($mock): Command {
+                $this->calls[] = [
+                    'object' => 'Command',
+                    'method' => 'createTable',
+                    'name' => $name,
+                    'table' => $table,
+                    'columns' => $columns,
+                    'refTable' => $refTable,
+                    'refColumns' => $refColumns,
+                    'delete' => $delete,
+                    'update' => $update,
+                ];
+
+                return $mock;
+            }
+        );
+
+        $mock->method('dropTableIfExists')->willReturnCallback(
+            function (string $table) use ($mock): Command {
+                $this->calls[] = [
+                    'object' => 'Command',
+                    'method' => 'dropTableIfExists',
+                    'table' => $table,
+                ];
+
+                return $mock;
+            }
+        );
+
+        $mock->method('execute')->willReturnCallback(
+            function (): int {
+                $this->calls[] = [
+                    'object' => 'Command',
+                    'method' => 'execute',
+                ];
+
+                return 123;
+            }
+        );
+
+        return $mock;
+    }
+
+    public function testFor(): void
+    {
+        self::assertSame(
+            MigrationContract::CRAFT,
+            $this->migration->for(),
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUpWhenTableExists(): void
+    {
+        $this->tableExistsReturn = true;
+
+        self::assertTrue($this->migration->up());
+
+        self::assertSame(
+            [
+                [
+                    'object' => 'DbConnection',
+                    'method' => 'tableExists',
+                    'table' => '{{%ansel_images}}',
+                ],
+            ],
+            $this->calls,
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testUpWhenTableDoesNotExist(): void
+    {
+        $this->tableExistsReturn = false;
+
+        self::assertTrue($this->migration->up());
+
+        self::assertCount(10, $this->calls);
+
+        self::assertSame(
+            [
+                'object' => 'DbConnection',
+                'method' => 'tableExists',
+                'table' => '{{%ansel_images}}',
+            ],
+            $this->calls[0],
+        );
+
+        self::assertSame(
+            'Command',
+            /** @phpstan-ignore-next-line */
+            $this->calls[1]['object'],
+        );
+
+        self::assertSame(
+            'createTable',
+            /** @phpstan-ignore-next-line */
+            $this->calls[1]['method'],
+        );
+
+        self::assertSame(
+            '{{%ansel_images}}',
+            /** @phpstan-ignore-next-line */
+            $this->calls[1]['table'],
+        );
+
+        /** @phpstan-ignore-next-line */
+        $columns = $this->calls[1]['columns'];
+
+        assert(is_array($columns));
+
+        self::assertCount(20, $columns);
+
+        $schemaId = $columns['id'];
+        assert($schemaId instanceof ColumnSchemaBuilder);
+        self::assertSame('pk', (string) $schemaId);
+
+        $schemaElementId = $columns['elementId'];
+        assert($schemaElementId instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer(11) NOT NULL',
+            (string) $schemaElementId,
+        );
+
+        $schemaFieldId = $columns['fieldId'];
+        assert($schemaFieldId instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer(11) NOT NULL',
+            (string) $schemaFieldId,
+        );
+
+        $schemaUserId = $columns['userId'];
+        assert($schemaUserId instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer(11) NOT NULL',
+            (string) $schemaUserId,
+        );
+
+        $schemaAssetId = $columns['assetId'];
+        assert($schemaAssetId instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer(11) NOT NULL',
+            (string) $schemaAssetId,
+        );
+
+        $schemaHighQualAssetId = $columns['highQualAssetId'];
+        assert($schemaHighQualAssetId instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer(11)',
+            (string) $schemaHighQualAssetId,
+        );
+
+        $schemaThumbAssetId = $columns['thumbAssetId'];
+        assert($schemaThumbAssetId instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer(11)',
+            (string) $schemaThumbAssetId,
+        );
+
+        $schemaOrigAssetId = $columns['originalAssetId'];
+        assert($schemaOrigAssetId instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer(11)',
+            (string) $schemaOrigAssetId,
+        );
+
+        $schemaWidth = $columns['width'];
+        assert($schemaWidth instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer NOT NULL',
+            (string) $schemaWidth,
+        );
+
+        $schemaHeight = $columns['height'];
+        assert($schemaHeight instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer NOT NULL',
+            (string) $schemaHeight,
+        );
+
+        $schemaX = $columns['x'];
+        assert($schemaX instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer NOT NULL',
+            (string) $schemaX,
+        );
+
+        $schemaY = $columns['y'];
+        assert($schemaY instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'integer NOT NULL',
+            (string) $schemaY,
+        );
+
+        $schemaTitle = $columns['title'];
+        assert($schemaTitle instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'string(255)',
+            (string) $schemaTitle,
+        );
+
+        $schemaCaption = $columns['caption'];
+        assert($schemaCaption instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'string(255)',
+            (string) $schemaCaption,
+        );
+
+        $schemaCover = $columns['cover'];
+        assert($schemaCover instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'tinyint(1) NOT NULL',
+            (string) $schemaCover,
+        );
+
+        $schemaPosition = $columns['position'];
+        assert($schemaPosition instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'tinyint(4) NOT NULL',
+            (string) $schemaPosition,
+        );
+
+        $disabled = $columns['disabled'];
+        assert($disabled instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'tinyint',
+            (string) $disabled,
+        );
+
+        $schemaDateCreated = $columns['dateCreated'];
+        assert($schemaDateCreated instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'datetime NOT NULL',
+            (string) $schemaDateCreated,
+        );
+
+        $schemaDateUpdated = $columns['dateUpdated'];
+        assert($schemaDateUpdated instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            'datetime NOT NULL',
+            (string) $schemaDateUpdated,
+        );
+
+        $schemaUid = $columns['uid'];
+        assert($schemaUid instanceof ColumnSchemaBuilder);
+        self::assertSame(
+            "char(36) NOT NULL DEFAULT '0'",
+            (string) $schemaUid,
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'execute',
+            ],
+            $this->calls[2],
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'createTable',
+                'name' => 'fooBarForeign',
+                'table' => '{{%ansel_images}}',
+                'columns' => ['elementId'],
+                'refTable' => '{{%elements}}',
+                'refColumns' => ['id'],
+                'delete' => 'RESTRICT',
+                'update' => 'RESTRICT',
+            ],
+            $this->calls[3],
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'createTable',
+                'name' => 'fooBarForeign',
+                'table' => '{{%anselImages}}',
+                'columns' => ['fieldId'],
+                'refTable' => '{{%fields}}',
+                'refColumns' => ['id'],
+                'delete' => 'RESTRICT',
+                'update' => 'RESTRICT',
+            ],
+            $this->calls[4],
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'createTable',
+                'name' => 'fooBarForeign',
+                'table' => '{{%anselImages}}',
+                'columns' => ['userId'],
+                'refTable' => '{{%users}}',
+                'refColumns' => ['id'],
+                'delete' => 'RESTRICT',
+                'update' => 'RESTRICT',
+            ],
+            $this->calls[5],
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'createTable',
+                'name' => 'fooBarForeign',
+                'table' => '{{%anselImages}}',
+                'columns' => ['assetId'],
+                'refTable' => '{{%assets}}',
+                'refColumns' => ['id'],
+                'delete' => 'SET NULL',
+                'update' => 'RESTRICT',
+            ],
+            $this->calls[6],
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'createTable',
+                'name' => 'fooBarForeign',
+                'table' => '{{%anselImages}}',
+                'columns' => ['highQualAssetId'],
+                'refTable' => '{{%assets}}',
+                'refColumns' => ['id'],
+                'delete' => 'SET NULL',
+                'update' => 'RESTRICT',
+            ],
+            $this->calls[7],
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'createTable',
+                'name' => 'fooBarForeign',
+                'table' => '{{%anselImages}}',
+                'columns' => ['thumbAssetId'],
+                'refTable' => '{{%assets}}',
+                'refColumns' => ['id'],
+                'delete' => 'SET NULL',
+                'update' => 'RESTRICT',
+            ],
+            $this->calls[8],
+        );
+
+        self::assertSame(
+            [
+                'object' => 'Command',
+                'method' => 'createTable',
+                'name' => 'fooBarForeign',
+                'table' => '{{%anselImages}}',
+                'columns' => ['originalAssetId'],
+                'refTable' => '{{%assets}}',
+                'refColumns' => ['id'],
+                'delete' => 'SET NULL',
+                'update' => 'RESTRICT',
+            ],
+            $this->calls[9],
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testDown(): void
+    {
+        self::assertTrue($this->migration->down());
+
+        self::assertSame(
+            [
+                [
+                    'object' => 'Command',
+                    'method' => 'dropTableIfExists',
+                    'table' => '{{%ansel_images}}',
+                ],
+                [
+                    'object' => 'Command',
+                    'method' => 'execute',
+                ],
+            ],
+            $this->calls,
+        );
+    }
+}
