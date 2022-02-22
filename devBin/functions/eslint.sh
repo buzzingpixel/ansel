4808dbbfef845ca32be03fc8e2bab830b8c98a10
TJ Draper <tj@buzzingpixel.com>
2022-02-21 21:33:41 -0600

    Set up github CI workflow

--- devBin/functions/eslint.sh
+++ devBin/functions/eslint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function eslint() {
-    yarn eslint --ext .js --ext .ts --ext .jsx --ext .tsx --ext .html --ext .vue --ext .mjs --no-error-on-unmatched-pattern assets/js assets/react/app;
+    yarn eslint --ext .js --ext .ts --ext .jsx --ext .tsx --ext .html --ext .vue --ext .mjs --no-error-on-unmatched-pattern assets/js assets/react/app --max-warnings=0;
 
     return 0;
 }
 
 function eslint-fix() {
-    yarn eslint --ext .js --ext .ts --ext .jsx --ext .tsx --ext .html --ext .vue --ext .mjs --no-error-on-unmatched-pattern assets/js assets/react/app --fix;
+    yarn eslint --ext .js --ext .ts --ext .jsx --ext .tsx --ext .html --ext .vue --ext .mjs --no-error-on-unmatched-pattern assets/js assets/react/app --fix --max-warnings=0;
 
     return 0;
 }
