17e512450b9c473ef49bb4f3079223a86257821d
TJ Draper <tj@buzzingpixel.com>
2022-03-29 08:53:16 -0500

    Add a "field is working" spinner/progress

--- assets/js/Field/Field.tsx
+++ assets/js/Field/Field.tsx
@@ -6,6 +6,7 @@ import { ImCrop } from 'react-icons/im';
 import { FiEdit } from 'react-icons/fi';
 import { CgEditBlackPoint } from 'react-icons/cg';
 import { MdDelete } from 'react-icons/md';
+import { SpinningCircles } from 'react-loading-icons';
 import FieldSettingsType from './FieldSettingsType';
 import CustomFieldType from './CustomFieldType';
 import FieldUploadSelect from './FieldUploadSelect';
@@ -30,6 +31,8 @@ const Field = (
         translations: Translations,
     },
 ) => {
+    const [processCount, setProcessCount] = useState(0);
+
     const [errorMessages, setErrorMessages] = useState({});
 
     const [images, setImages] = useState<Array<Image>>([]);
@@ -86,95 +89,106 @@ const Field = (
     }
 
     return (
-        <div
-            className="ansel_bg-gray-50 ansel_border ansel_border-gray-200 ansel_border-solid ansel_relative"
-            {...getRootProps()}
-        >
-            { isDragActive && <DragInProgress /> }
-            {
-                Object.keys(errorMessages).map((errorKey) => (
-                    <FieldError errorMessage={errorMessages[errorKey]} />
-                ))
-            }
-            <input {...getInputProps()} />
-            <div className="ansel_p-4">
-                <div className={uploaderClass}>
-                    <FieldUploadSelect dropZoneOpenDeviceDialog={open} />
-                </div>
-                <ul
-                    role="list"
-                    className="ansel_grid ansel_grid-cols-1 ansel_gap-6 md:ansel_grid-cols-2 2xl:ansel_grid-cols-3 3xl:ansel_grid-cols-4 ansel_p-4"
-                >
-                    {images.map((image, index) => (
-                        <li
-                            key={index}
-                            className="ansel_col-span-1 ansel_flex ansel_flex-col ansel_text-center ansel_bg-white ansel_rounded-lg ansel_shadow ansel_divide-y ansel_divide-gray-200"
-                        >
-                            <div className="ansel_flex-1 ansel_flex ansel_flex-col ansel_p-8">
-                                <img
-                                    className="ansel_w-48 ansel_flex-shrink-0 ansel_mx-auto"
-                                    src={image.imageUrl}
-                                    alt=""
-                                />
-                            </div>
-                            <div>
-                                <div className="ansel_-mt-px ansel_flex ansel_border-0 ansel_border-t ansel_border-gray-200 ansel_border-solid">
-                                    <div className="ansel_w-0 ansel_flex-1 ansel_flex">
-                                        <a
-                                            href="#"
-                                            className="ansel_relative ansel_-mr-px w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium border ansel_border-transparent ansel_rounded-bl-lg hover:ansel_bg-gray-200"
-                                        >
-                                            <IconContext.Provider value={{ color: '#525252' }}>
-                                                <FiEdit className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
-                                                <span className="ansel_sr-only">Edit Fields</span>
-                                            </IconContext.Provider>
-                                        </a>
-                                    </div>
-                                    <div className="ansel_-ml-px ansel_w-0 ansel_flex-1 ansel_flex ansel_border-0 ansel_border-l ansel_border-gray-200 ansel_border-solid">
-                                        <a
-                                            href="#"
-                                            className="ansel_relative ansel_w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium ansel_border ansel_border-transparent hover:ansel_text-gray-500 hover:ansel_bg-gray-200"
-                                        >
-                                            <IconContext.Provider value={{ color: '#525252' }}>
-                                                <ImCrop className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
-                                                <span className="ansel_sr-only">Edit Image</span>
-                                            </IconContext.Provider>
-                                        </a>
-                                    </div>
-                                    <div className="ansel_-ml-px ansel_w-0 ansel_flex-1 ansel_flex ansel_border-0 ansel_border-l ansel_border-gray-200 ansel_border-solid">
-                                        <a
-                                            href="#"
-                                            className="ansel_relative ansel_w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium ansel_border ansel_border-transparent hover:ansel_text-gray-500 hover:ansel_bg-gray-200"
-                                        >
-                                            <IconContext.Provider value={{ color: '#525252' }}>
-                                                <CgEditBlackPoint className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
-                                                <span className="ansel_sr-only">Edit Focal Point</span>
-                                            </IconContext.Provider>
-                                        </a>
-                                    </div>
-                                    <div className="ansel_-ml-px ansel_w-0 ansel_flex-1 ansel_flex ansel_border-0 ansel_border-l ansel_border-gray-200 ansel_border-solid">
-                                        <a
-                                            href="#"
-                                            className="ansel_relative ansel_w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium ansel_border ansel_border-transparent ansel_rounded-br-lg hover:ansel_text-gray-500 hover:ansel_bg-gray-200"
-                                        >
-                                            <IconContext.Provider value={{ color: '#525252' }}>
-                                                <MdDelete className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
-                                                <span className="ansel_sr-only">Remove Image</span>
-                                            </IconContext.Provider>
-                                        </a>
+        <>
+            <div className={processCount > 0 ? 'ansel_opacity-1' : 'ansel_opacity-0'}>
+                <SpinningCircles
+                    fill="#000"
+                    stroke="#cdcdcd"
+                    speed={2}
+                    width={20}
+                    height={20}
+                />
+            </div>
+            <div
+                className="ansel_bg-gray-50 ansel_border ansel_border-gray-200 ansel_border-solid ansel_relative"
+                {...getRootProps()}
+            >
+                { isDragActive && <DragInProgress /> }
+                {
+                    Object.keys(errorMessages).map((errorKey) => (
+                        <FieldError errorMessage={errorMessages[errorKey]} />
+                    ))
+                }
+                <input {...getInputProps()} />
+                <div className="ansel_p-4">
+                    <div className={uploaderClass}>
+                        <FieldUploadSelect dropZoneOpenDeviceDialog={open} />
+                    </div>
+                    <ul
+                        role="list"
+                        className="ansel_grid ansel_grid-cols-1 ansel_gap-6 md:ansel_grid-cols-2 2xl:ansel_grid-cols-3 3xl:ansel_grid-cols-4 ansel_p-4"
+                    >
+                        {images.map((image, index) => (
+                            <li
+                                key={index}
+                                className="ansel_col-span-1 ansel_flex ansel_flex-col ansel_text-center ansel_bg-white ansel_rounded-lg ansel_shadow ansel_divide-y ansel_divide-gray-200"
+                            >
+                                <div className="ansel_flex-1 ansel_flex ansel_flex-col ansel_p-8">
+                                    <img
+                                        className="ansel_w-48 ansel_flex-shrink-0 ansel_mx-auto"
+                                        src={image.imageUrl}
+                                        alt=""
+                                    />
+                                </div>
+                                <div>
+                                    <div className="ansel_-mt-px ansel_flex ansel_border-0 ansel_border-t ansel_border-gray-200 ansel_border-solid">
+                                        <div className="ansel_w-0 ansel_flex-1 ansel_flex">
+                                            <a
+                                                href="#"
+                                                className="ansel_relative ansel_-mr-px w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium border ansel_border-transparent ansel_rounded-bl-lg hover:ansel_bg-gray-200"
+                                            >
+                                                <IconContext.Provider value={{ color: '#525252' }}>
+                                                    <FiEdit className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
+                                                    <span className="ansel_sr-only">Edit Fields</span>
+                                                </IconContext.Provider>
+                                            </a>
+                                        </div>
+                                        <div className="ansel_-ml-px ansel_w-0 ansel_flex-1 ansel_flex ansel_border-0 ansel_border-l ansel_border-gray-200 ansel_border-solid">
+                                            <a
+                                                href="#"
+                                                className="ansel_relative ansel_w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium ansel_border ansel_border-transparent hover:ansel_text-gray-500 hover:ansel_bg-gray-200"
+                                            >
+                                                <IconContext.Provider value={{ color: '#525252' }}>
+                                                    <ImCrop className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
+                                                    <span className="ansel_sr-only">Edit Image</span>
+                                                </IconContext.Provider>
+                                            </a>
+                                        </div>
+                                        <div className="ansel_-ml-px ansel_w-0 ansel_flex-1 ansel_flex ansel_border-0 ansel_border-l ansel_border-gray-200 ansel_border-solid">
+                                            <a
+                                                href="#"
+                                                className="ansel_relative ansel_w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium ansel_border ansel_border-transparent hover:ansel_text-gray-500 hover:ansel_bg-gray-200"
+                                            >
+                                                <IconContext.Provider value={{ color: '#525252' }}>
+                                                    <CgEditBlackPoint className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
+                                                    <span className="ansel_sr-only">Edit Focal Point</span>
+                                                </IconContext.Provider>
+                                            </a>
+                                        </div>
+                                        <div className="ansel_-ml-px ansel_w-0 ansel_flex-1 ansel_flex ansel_border-0 ansel_border-l ansel_border-gray-200 ansel_border-solid">
+                                            <a
+                                                href="#"
+                                                className="ansel_relative ansel_w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium ansel_border ansel_border-transparent ansel_rounded-br-lg hover:ansel_text-gray-500 hover:ansel_bg-gray-200"
+                                            >
+                                                <IconContext.Provider value={{ color: '#525252' }}>
+                                                    <MdDelete className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
+                                                    <span className="ansel_sr-only">Remove Image</span>
+                                                </IconContext.Provider>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-                {images.length > 4
-                    && <div className="ansel_pt-6">
-                        <FieldUploadSelect />
-                    </div>
-                }
+                            </li>
+                        ))}
+                    </ul>
+                    {images.length > 4
+                        && <div className="ansel_pt-6">
+                            <FieldUploadSelect />
+                        </div>
+                    }
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
