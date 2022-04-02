ec6b3c481f53d9d98d89daebc25e5380e09df80f
TJ Draper <tj@buzzingpixel.com>
2022-04-02 15:24:46 -0500

    Create classes for getting modal link (EE)

---
+++ src/Field/Field/FileChooserModalLink/EeModalLink.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BuzzingPixel\Ansel\Field\Field\FileChooserModalLink;
+
+use BuzzingPixel\Ansel\Field\Settings\FieldSettingsCollection;
+use BuzzingPixel\Ansel\Translations\TranslatorContract;
+use ExpressionEngine\Addons\FilePicker\Service\FilePicker\Factory;
+use ExpressionEngine\Model\File\UploadDestination;
+use ExpressionEngine\Service\Model\Facade as RecordService;
+
+use function assert;
+
+class EeModalLink
+{
+    private Factory $filePickerFactory;
+
+    private RecordService $recordService;
+    private TranslatorContract $translator;
+
+    public function __construct(
+        Factory $filePickerFactory,
+        RecordService $recordService,
+        TranslatorContract $translator
+    ) {
+        $this->recordService     = $recordService;
+        $this->filePickerFactory = $filePickerFactory;
+        $this->translator        = $translator;
+    }
+
+    public function getLink(FieldSettingsCollection $fieldSettings): string
+    {
+        $directory = $this->recordService->get('UploadDestination')
+            ->filter(
+                'id',
+                $fieldSettings->uploadLocation()->directoryId(),
+            )
+            ->first();
+
+        assert($directory instanceof UploadDestination);
+
+        /** @phpstan-ignore-next-line */
+        $modalView = $directory->getProperty('default_modal_view');
+
+        $picker = $this->filePickerFactory->make(
+            $fieldSettings->uploadLocation()->directoryId(),
+        );
+
+        $link = $picker->getLink(
+            $this->translator->getLine('choose_an_existing_image'),
+        )
+            ->enableFilters()
+            ->enableUploads();
+
+        if ($modalView === 'list') {
+            $link->asList();
+        } elseif ($modalView === 'thumb') {
+            $link->asThumbs();
+        }
+
+        return $link->render();
+    }
+}
---
+++ src/Field/Field/FileChooserModalLink/FileChooserModalLinkFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BuzzingPixel\Ansel\Field\Field\FileChooserModalLink;
+
+use BuzzingPixel\Ansel\Field\Settings\FieldSettingsCollection;
+
+class FileChooserModalLinkFactory
+{
+    private EeModalLink $eeModalLink;
+
+    private AssetsModalLink $assetsModalLink;
+
+    private TreasuryModalLink $treasuryModalLink;
+
+    public function __construct(
+        EeModalLink $eeModalLink,
+        AssetsModalLink $assetsModalLink,
+        TreasuryModalLink $treasuryModalLink
+    ) {
+        $this->eeModalLink       = $eeModalLink;
+        $this->assetsModalLink   = $assetsModalLink;
+        $this->treasuryModalLink = $treasuryModalLink;
+    }
+
+    public function getLink(FieldSettingsCollection $fieldSettings): string
+    {
+        $uploadLocation = $fieldSettings->uploadLocation();
+
+        switch ($uploadLocation->directoryType()) {
+            case 'ee':
+                return $this->eeModalLink->getLink(
+                    $fieldSettings,
+                );
+
+            case 'treasury':
+                return $this->treasuryModalLink->getLink(
+                    $fieldSettings,
+                );
+
+            case 'assets':
+                return $this->assetsModalLink->getLink(
+                    $fieldSettings,
+                );
+        }
+
+        return '';
+    }
+}
