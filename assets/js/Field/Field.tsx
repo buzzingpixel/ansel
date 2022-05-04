bf72bb1fe62e8094fa565f53633313182f6e0690
TJ Draper <tj@buzzingpixel.com>
2022-05-03 21:43:30 -0500

    Get delete button working

--- assets/js/Field/Field.tsx
+++ assets/js/Field/Field.tsx
@@ -14,6 +14,7 @@ import OnDropAccepted from './DropHandlers/DropAccepted/OnDropAccepted';
 import FieldStateType from './Types/FieldStateType';
 import FieldDataType from './Types/FieldDataType';
 import WorkingIndicator from './WorkingIndicator';
+import DeleteButton from './InteractionHandlers/DeleteButton';
 
 const Field = (fieldData: FieldDataType) => {
     const [fieldState, setFieldState] = useState<FieldStateType>({
@@ -148,17 +149,7 @@ const Field = (fieldData: FieldDataType) => {
                                                 </IconContext.Provider>
                                             </a>
                                         </div>
-                                        <div className="ansel_-ml-px ansel_w-0 ansel_flex-1 ansel_flex ansel_border-0 ansel_border-l ansel_border-gray-200 ansel_border-solid">
-                                            <a
-                                                href="#"
-                                                className="ansel_relative ansel_w-0 ansel_flex-1 ansel_inline-flex ansel_items-center ansel_justify-center ansel_py-4 ansel_text-sm ansel_text-gray-700 ansel_font-medium ansel_border ansel_border-transparent ansel_rounded-br-lg hover:ansel_text-gray-500 hover:ansel_bg-gray-200"
-                                            >
-                                                <IconContext.Provider value={{ color: '#525252' }}>
-                                                    <MdDelete className="ansel_w-5 ansel_h-5 ansel_text-gray-400" aria-hidden="true" />
-                                                    <span className="ansel_sr-only">Remove Image</span>
-                                                </IconContext.Provider>
-                                            </a>
-                                        </div>
+                                        <DeleteButton index={index} setFieldState={setFieldState} />
                                     </div>
                                 </div>
                             </li>
